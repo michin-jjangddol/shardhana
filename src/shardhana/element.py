b0208a18dabe1c335c37e2ee549c6a527956adab
michin-jjangddol <leeseok.str.eng@gmail.com>
2026-03-08 12:05:47 +0900

    core: add Node class and initial HEM element structure

---
+++ src/shardhana/element.py
@@ -0,0 +1,98 @@
+# src/shardhana/element.py
+
+"""
+Shardhana Element definition
+
+Basic concept
+
+Element = Node + Seed + Space + State + Relations
+
+Node      : Represents the position of an element.
+Seed      : Material core of the element
+Space     : Surrounding interaction space
+State     : Time-dependent variables
+Relations : Dynamic relationship between neighboring elements
+"""
+
+from .node import Node
+
+
+class Seed:
+    """
+    Structural core of the element
+    """
+
+    def __init__(self, density=0.0, volume=0.0, shape=None):
+
+        self.density = density
+        self.volume = volume
+        self.shape = shape
+
+
+class Space:
+    """
+    Space surrounding the seed
+    """
+
+    def __init__(self, density=0.0, volume=0.0, shape=None):
+
+        self.density = density
+        self.volume = volume
+        self.shape = shape
+
+
+class State:
+    """
+    Time-dependent state variables
+    """
+
+    def __init__(self):
+
+        self.pressure = 0.0
+        self.temperature = 0.0
+
+        self.velocity = 0.0
+        self.acceleration = 0.0
+
+        self.moisture = 0.0
+
+
+class Relation:
+    """
+    Relation between elements.
+
+    Represents the spatial and physical relationship
+    between two neighboring elements.
+
+    Relations may include relative position, distance,
+    contact state, or connection state.
+
+    Unlike FEM connectivity, relations are not fixed
+    and may change over time.
+    """
+
+    def __init__(self):
+
+        self.target = None
+        self.relative_vector = None
+        self.distance = 0.0
+
+        self.connected = False
+        self.contact = False
+
+
+class Element:
+    """
+    Basic HEM element
+
+    Element = Node + Seed + Space + State + Relations
+    """
+
+    def __init__(self):
+
+        self.node = Node()
+        self.seed = Seed()
+        self.space = Space()
+        self.state = State()
+
+        self.relations = []
