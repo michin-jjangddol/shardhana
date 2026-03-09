74009c58d414a32fc8c7e2b3c9ec388d5e1a76a1
michin-jjangddol <leeseok.str.eng@gmail.com>
2026-03-09 21:45:05 +0900

    Add basic Material container class

---
+++ src/shardhana/material.py
@@ -0,0 +1,17 @@
+# src/shardhana/material.py
+
+"""
+Material definition for Shardhana
+"""
+
+
+class Material:
+    """
+    Basic material properties
+    """
+
+    def __init__(self):
+
+        self.density = 0.0
+        self.young_modulus = 0.0
+        self.poisson_ratio = 0.0
