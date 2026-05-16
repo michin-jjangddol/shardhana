bdf9aab04a53b7c4e83c3f8888799eec7d0b38db
michin-jjangddol <leeseok.str.eng@gmail.com>
2026-05-16 21:29:26 +0900

    Refactor material structure into src core package

--- src/shardhana/core/material.py
+++ src/shardhana/core/material.py
@@ -1,3 +1,33 @@
-'''
+"""
+core/material.py
 재료 특성 구조
-'''
+
+Material definition for Shardhana
+
+Core material structure
+"""
+
+class Material:
+    """
+    Basic material properties
+    """
+
+    def __init__(
+        self,
+        name: str = "material",
+        density: float = 0.0,
+        young_modulus: float = 0.0,
+        poisson_ratio: float = 0.0,
+    ):
+
+        # material name
+        self.name = name
+
+        # physical properties
+        self.density = float(density)
+
+        self.young_modulus = float(young_modulus)
+        self.poisson_ratio = float(poisson_ratio)
+
+        # short alias
+        self.E = self.young_modulus
